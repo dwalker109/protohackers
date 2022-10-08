3fbd9a409f3c4496e6122e1874b69934b9f80303
Dan Walker <dwalker109@gmail.com>
2022-10-08 02:49:29 +0100

    Completed 1: Prime Time

--- prime_time/src/main.rs
+++ prime_time/src/main.rs
@@ -1,4 +1,5 @@
 use std::io::prelude::*;
+use std::io::BufReader;
 use std::net::TcpListener;
 use std::thread;
 
@@ -9,13 +10,37 @@ fn main() -> std::io::Result<()> {
         thread::spawn(move || {
             eprintln!("Opening connection");
 
-            let mut stream = stream.expect("open incoming TCP stream");
-            let mut buf = Vec::new();
+            let mut stream = stream.expect("opened TCP stream");
+            let mut reader = BufReader::new(stream.try_clone().expect("cloned stream"));
+            let mut buf = String::new();
 
-            while let Ok(bytes_read) = stream.read_to_end(&mut buf) {
+            while let Ok(bytes_read) = reader.read_line(&mut buf) {
+                if bytes_read == 0 {
+                    eprintln!("No bytes read");
+                    break;
+                }
+
                 eprintln!("Got {bytes_read} bytes: {buf:?}");
+                eprintln!("{buf}");
 
-                let json = serde_json::from_slice::<PrimeRequest>(&buf);
+                let json: serde_json::Value =
+                    match serde_json::from_str::<PrimeRequest>(buf.trim_end()) {
+                        Ok(data) if data.method == "isPrime" => {
+                            eprintln!("Struct: {data:?}");
+                            serde_json::json!({
+                                "method": "isPrime",
+                                "prime": primes::is_prime(data.number.as_u64().unwrap_or_default())
+                            })
+                        }
+                        _ => {
+                            eprintln!("Bad request: {buf:?}");
+                            serde_json::json!("malformed")
+                        }
+                    };
+
+                let res_str = serde_json::to_string(&json).expect("infallible");
+                writeln!(stream, "{res_str}").ok();
+                buf.clear();
             }
 
             eprintln!("Closing connection");
@@ -25,14 +50,8 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-#[derive(serde::Deserialize)]
+#[derive(Debug, serde::Deserialize)]
 struct PrimeRequest {
     method: String,
     number: serde_json::Number,
-}
-
-#[derive(serde::Serialize)]
-struct PrimeResponse {
-    method: &'static str,
-    prime: bool,
 }
